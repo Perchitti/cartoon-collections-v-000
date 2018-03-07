cd125d8866a4c8460ccbd17e54e2512ae5f2e1cb
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-07 15:43:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,7 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.include?(cheese_types)
+  cheese_types.find do |new_array|
+  cheese_types.include?(new_array)
+end
 end
