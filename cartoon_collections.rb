63cc6792d9f511ab8b77ad68ad54c9bf3afb2ef0
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-07 15:20:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  dwarves.each.with_index(0) do |name, index|
+  dwarves.each.with_index(1) do |name, index|
     puts "#{index}. #{name}"
 end
 end
